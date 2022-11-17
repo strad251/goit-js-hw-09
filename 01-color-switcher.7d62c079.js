b229919bc63b03f991c8628305d104490c0e555a
strad251 <60315122+strad251@users.noreply.github.com>
2022-11-17 22:17:39 +0000

    Deploying to gh-pages from @ strad251/goit-js-hw-09@ac7fe7c42412e85c5944c4909faf680044469504 🚀

---
+++ 01-color-switcher.7d62c079.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=null;e.disabled=!0,t.addEventListener("click",(function(t){d=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!1,t.target.disabled=!e.disabled})),e.addEventListener("click",(function(n){clearInterval(d),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.7d62c079.js.map
